576ce625f2124ebddff5b567bc570d6e11bdaf8b
unknown <gavrilasflorin@yahoo.com>
2023-11-12 21:55:53 +0200

    Adaugare functie inregistrare angajat si lista angajati

---
+++ IntroducereAngajatNou.py
@@ -0,0 +1,25 @@
+import csv 
+import os 
+
+CALE = 'temacurs16/temacurs16/'
+fisier_angajati = 'angajati.csv'
+
+numeColoana = 'Nume'
+
+def introducere_angajat():
+    
+    nume = input('Introduceti numele angajatului: ')
+    prenume = input('Introduceti prenumele angajatului: ')
+    salariul = input('Introduceti salariul angajatului: ')
+
+    with open(CALE + fisier_angajati, 'a' , newline='') as file:
+        writer=csv.writer(file)
+
+        if file.tell() == 0:
+            writer.writerow([numeColoana,'Prenume','Salariu'])
+
+        writer.writerow([nume,prenume,salariul])
+
+        print(f'Angajatul {nume} {prenume} a fost adaugat cu succes')
+
+introducere_angajat()
